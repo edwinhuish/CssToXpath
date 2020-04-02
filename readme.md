49e4e3ae0d8a4b7e785b265f6ce9fe14d61450d5
Edwin Xu <edwinhuish@gmail.com>
2020-04-02 22:51:08 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,84 @@
+# CssToPath
+
+This is a library to translate various CSS selectors to their XPath equivalent.
+
+## Example
+
+```php
+
+// Will output //*[contains(concat(' ', normalize-space(@class), ' '), ' h ')]
+echo CssToXpath::transform('.note');
+
+// Will output //*[@id='someid']
+echo $translator->translate('#someid');
+
+```
+
+## Installation
+
+You can add this library as a local, per-project dependency to your project using Composer:
+
+```
+composer require edwinhuish/css-to-xpath
+```
+
+## Selectors supported
+
+The following selectors are currently covered:
+
+- #id
+- tag#id
+- tag #id
+- tag
+- tag tag
+- tag > tag
+- tag + tag
+- tag ~ tag
+- tag, tag
+- .classname
+- tag.classname
+- tag .classname
+- tag.classname, tag.classname
+- tag.classname tag.classname
+- tag.classname > tag.classname
+- tag#id + tag > tag
+- tag[id]:contains(Selectors)
+- tag[attribute][attribute]
+- tag[attribute]
+- tag[attribute=example]
+- tag[attribute^=exa]
+- tag[class$=mple]
+- tag[attribute*=e]
+- tag[attribute|=dialog]
+- tag[attribute!=made_up]
+- tag[attribute!="made_up"]
+- tag[attribute~=example]
+- tag:not(.classname)
+- tag:contains(selectors)
+- tag:nth-child(n)
+- tag:nth-child(even)
+- tag:nth-child(odd)
+- tag:nth-child(3n+8)
+- tag:nth-child(2n+1)
+- tag:nth-child(3)
+- tag:nth-child(4n)
+- tag:only-child
+- tag:last-child
+- tag:first-child
+- foo|bar
+- tag[attribute^=exa][attribute$=mple]
+- :empty
+- :root
+- :even
+- :odd
+- :first-child
+- :last-child
+- :only-child
+- :parent
+- :first
+- :last
+- :header
+- :not(foo)
+- :has(foo)
+- :contains(foo)
+
---
+++ tests/TranslateTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Edwinhuish\CssToXpath\Tests;
+
+use Edwinhuish\CssToXpath\CssToXpath;
+use PHPUnit\Framework\TestCase;
+
+class TranslateTest extends TestCase
+{
+
+    public function testSelectors()
+    {
+        $css_to_xpath = array(
+            'a'                           => '//a',
+            '*'                           => '//*',
+            '* > a'                       => '//*/a',
+            '#someid'                     => '//*[@id=\'someid\']',
+            'p#someid'                    => '//p[@id=\'someid\']',
+            '#some\\.id'                  => '//*[@id=\'some.id\']',
+            '#someid.s-class'             => '//*[@id=\'someid\'][contains(concat(\' \', normalize-space(@class), \' \'), \' s-class \')]',
+            '#id[_]'                      => '//*[@id=\'id\'][@_]',
+            'p a'                         => '//p//a',
+            'div, span'                   => '//div|//span',
+            'a[href]'                     => '//a[@href]',
+            'a[href][rel]'                => '//a[@href][@rel]',
+            'a[href="html"]'              => '//a[@href=\'html\']',
+            'a[href!="html"]'             => '//a[@href!=\'html\']',
+            'a[href*=\'html\']'           => '//a[contains(@href, \'html\')]',
+            '[href*=\'html\']'            => '//*[contains(@href, \'html\')]',
+            '[href^=\'html\']'            => '//*[starts-with(@href, \'html\')]',
+            'meta[http-equiv^="Content"]' => '//meta[starts-with(@http-equiv, \'Content\')]',
+            'meta[http-equiv^=Content]'   => '//meta[starts-with(@http-equiv, \'Content\')]',
+            '[href$=\'html\']'            => '//*[@href and substring(@href, string-length(@href)-3) = \'html\']',
+            '[href~=\'html\']'            => '//*[contains(concat(\' \', normalize-space(@href), \' \'), \' html \')]',
+            '[href|=\'html\']'            => '//*[@href=\'html\' or starts-with(@href, \'html-\')]',
+            '> a'                         => '/a',
+            'p > a'                       => '//p/a',
+            'p> a'                        => '//p/a',
+            'p>a'                         => '//p/a',
+            'p > a[href]'                 => '//p/a[@href]',
+            'p a[href]'                   => '//p//a[@href]',
+            ':disabled'                   => '//*[@disabled]',
+            'div :header'                 => '//div//*[self::h1 or self::h2 or self::h3 or self::h4 or self::h5 or self::h6]',
+            ':odd'                        => '//*[position() mod 2 = 0]',
+            '.h'                          => '//*[contains(concat(\' \', normalize-space(@class), \' \'), \' h \')]',
+            '.😾-_😾'                     => '//*[contains(concat(\' \', normalize-space(@class), \' \'), \' 😾-_😾 \')]',
+            '.hidden'                     => '//*[contains(concat(\' \', normalize-space(@class), \' \'), \' hidden \')]',
+            '.hidden-something'           => '//*[contains(concat(\' \', normalize-space(@class), \' \'), \' hidden-something \')]',
+            'a.hidden[href]'              => '//a[contains(concat(\' \', normalize-space(@class), \' \'), \' hidden \')][@href]',
+            'a[href] > .hidden'           => '//a[@href]/*[contains(concat(\' \', normalize-space(@class), \' \'), \' hidden \')]',
+            'a:not(b[co-ol])'             => '//a[not(self::b[@co-ol])]',
+            'a:not(b,c)'                  => '//a[not(self::b or self::c)]',
+            'a:not(.cool)'                => '//a[not(self::*[contains(concat(\' \', normalize-space(@class), \' \'), \' cool \')])]',
+            'a:contains(txt)'             => '//a[text()[contains(.,\'txt\')]]',
+            'h1 ~ ul'                     => '//h1/following-sibling::ul',
+            'h1 + ul'                     => '//h1/following-sibling::ul[preceding-sibling::*[1][self::h1]]',
+            'h1 ~ #id'                    => '//h1/following-sibling::*[@id=\'id\']',
+            'p > a:has(> a)'              => '//p/a[child::a]',
+            'p > a:has(>a)'               => '//p/a[child::a]',
+            'p > a:has(b > a)'            => '//p/a[descendant::b/a]',
+            'p > a:has(b> a)'             => '//p/a[descendant::b/a]',
+            'p > a:has(b>a)'              => '//p/a[descendant::b/a]',
+            'p>a:has(b>a)'                => '//p/a[descendant::b/a]',
+            'p > a:has(a)'                => '//p/a[descendant::a]',
+            'a:has(b)'                    => '//a[descendant::b]',
+            'a:first-child:first'         => '(//a[not(preceding-sibling::*)])[1]',
+            'div > a:first'               => '(//div/a)[1]',
+            ':first'                      => '(//*)[1]',
+            'img:nth-child(0)'            => '//img',
+            'img:nth-child(234)'          => '//img[234]',
+            'img:nth-child(-3)'           => '//img[last()-2]',
+        );
+
+        foreach ($css_to_xpath as $css => $expected_xpath) {
+            $this->assertEquals($expected_xpath, CssToXpath::transform($css), $css);
+        }
+    }
+}
